15ae94ab87d9b0f58bd62093bdf7530adca84c81
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-02 01:21:04 +0200

    Change wss declaration to www

--- server/wsserver.js
+++ server/wsserver.js
@@ -1,29 +1,30 @@
+const server = require('./bin/www');
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 });
+let wss = null; //= new WebSocket.Server({ server });
 
-function heartbeat() {
-  this.isAlive = true;
-}
+// function heartbeat() {
+//   this.isAlive = true;
+// }
 
-wss.on('connection', ws => {
-  ws.isAlive = true;
-  ws.on('error', console.error);
-  ws.on('pong', heartbeat);
+// wss.on('connection', ws => {
+//   ws.isAlive = true;
+//   ws.on('error', console.error);
+//   ws.on('pong', heartbeat);
   
-  console.log("New connection.");
-});
+//   console.log("New connection.");
+// });
 
-const interval = setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
+// const interval = setInterval(function ping() {
+//   wss.clients.forEach(function each(ws) {
+//     if (ws.isAlive === false) return ws.terminate();
     
-    ws.isAlive = false;
-    ws.ping();
-  });
-}, 30000);
+//     ws.isAlive = false;
+//     ws.ping();
+//   });
+// }, 30000);
 
-wss.on('close', function  close() {
-  clearInterval(interval);
-});
+// wss.on('close', function  close() {
+//   clearInterval(interval);
+// });
 
 module.exports = wss;
