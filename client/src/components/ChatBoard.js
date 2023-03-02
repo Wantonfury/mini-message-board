5cd554a9ae1c869f016d99b4643924467079902f
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-02 19:52:01 +0200

    Add potential workaround for ports

--- client/src/components/ChatBoard.js
+++ client/src/components/ChatBoard.js
@@ -1,7 +1,21 @@
 import '../css/ChatBoard.css';
-import { useCallback } from "react";
+import { useCallback, useState, useEffect } from "react";
 
 const ChatBoard = (props) => {
+  const [messagesCount, setMessagesCount] = useState(20);
+  const [chatWindow, setChatWindow] = useState(null);
+  
+  if (!chatWindow && document.querySelector('.chat-list')) setChatWindow(document.querySelector('.chat-list'));
+  
+  useEffect(() => {
+    setChatWindow(document.querySelector('.chat-list'));
+  }, []);
+  
+  useEffect(() => {
+    console.log(chatWindow.scrollY);
+    if (!chatWindow.scrollY) setMessagesCount(count => count + 10);
+  }, [chatWindow.scrollY]);
+  
   var stringToColor = (string, saturation = 100, lightness = 75) => {
     let hash = 0;
     for (let i = 0; i < string.length; i++) {
@@ -12,14 +26,14 @@ const ChatBoard = (props) => {
   }
   
   const displayMessages = useCallback(() => {
-    return props.messages.map((message, index) => {
+    return props.messages.slice(0, messagesCount).map((message, index) => {
       return <li key={index}>
         <span className="chat-date">{(message.date ? '(' + message.date + ') ' : '')}</span>
         <span className='chat-user' style={{ color: stringToColor(message.user) }}>{message.user + ': '}</span>
         <span className='chat-text'>{message.text}</span>
       </li>
     });
-  }, [props.messages]);
+  }, [props.messages, messagesCount]);
   
   return (
     <ul className="chat-list">
